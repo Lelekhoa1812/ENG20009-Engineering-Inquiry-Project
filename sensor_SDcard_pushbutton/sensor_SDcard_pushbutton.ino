c7428102c02dd6e53c58db175a2b550bf03e0dc1
Khoa Le <120232418+Lelekhoa1812@users.noreply.github.com>
2024-09-01 16:10:09 +1000

    Add files via upload

--- sensor_SDcard_pushbutton/sensor_SDcard_pushbutton.ino
+++ sensor_SDcard_pushbutton/sensor_SDcard_pushbutton.ino
@@ -1,16 +1,11 @@
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BME680.h"
 #include <BH1750.h>
-#include "RTClib.h" 
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
 #include <Adafruit_GFX.h>
 #include <SdFat.h>
-
-RTC_DS1307 rtc;
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}; //create a matrix for displaying the date
-DateTime current;
 
 SdFs sd;
 FsFile file;
@@ -19,14 +14,8 @@ const int button1 = 2;
 const int button2 = 3;
 const int button3 = 4;
 const int button4 = 5;
+const int SD_CS = 9;
 
-const uint8_t SD_CS_PIN = A3;
-const uint8_t SOFT_MISO_PIN = 12;
-const uint8_t SOFT_MOSI_PIN = 11;
-const uint8_t SOFT_SCK_PIN  = 13;
-SoftSpiDriver<SOFT_MISO_PIN, SOFT_MOSI_PIN, SOFT_SCK_PIN> softSpi;
-
-#define SD_CONFIG SdSpiConfig(SD_CS_PIN, SHARED_SPI, SD_SCK_MHZ(0), &softSpi)
 
 #define TFT_CS    10
 #define TFT_RST   6 
@@ -52,19 +41,9 @@ Adafruit_BME680 bme; // I2C
 void setup() {
   Serial.begin(9600);
 
-  rtc.begin();    
-  // Adjusting to Melbourne GMT+11 time.
-  rtc.adjust(DateTime(2023,05,18,10,39,30)); 
-
   while (!Serial);
   Serial.println(F("BME680 test"));
   Serial.println(F("BH1750 Test begin"));
-
-  if (!sd.begin(SD_CONFIG)) {
-    Serial.println("SD card initialization failed!");
-    sd.initErrorHalt();
-    while (1);
-  }
 
   if (!bme.begin(0x76)) {
     Serial.println("Could not find a valid BME680 sensor, check wiring!");
@@ -89,126 +68,74 @@ pinMode(button1, INPUT_PULLUP);
 pinMode(button2, INPUT_PULLUP);
 pinMode(button3, INPUT_PULLUP);
 pinMode(button4, INPUT_PULLUP);
-
-  tft.setCursor(5, 10);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.println("B1: BH1750 sensor");
-  tft.setCursor(5, 30);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.println("B2: BME680 sensor");
-  tft.setCursor(5, 50);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.println("B3: Both sensor");
-  tft.setCursor(5, 70);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.println("B4: Save Data to SD card"); 
-
-  if (!file.open("sensor_data.txt", O_RDWR | O_CREAT)) {
-    sd.errorHalt(F("open failed"));
-  }
-
-  file.close(); //release file
+if (!SD.begin(SD_CS)) {
+  Serial.println("SD card initialization failed!");
+  while (1);
 }
 
-String getAQData() {
-bme.performReading();
-
-  String data = "Temperature = " + String(bme.temperature) + " *C\n" +
-                "Pressure = " + String(bme.pressure / 100.0) + " hPa\n" +
-                "Humidity = " + String(bme.humidity) + " %\n" +
-                "Gas = " + String(bme.gas_resistance / 1000.0) + " KOhms\n" +
-                "Approx. Altitude = " + String(bme.readAltitude(SEALEVELPRESSURE_HPA)) + " m";
-
-  return data;
-
-}
-
-void display(DateTime now){
-  Serial.print(now.year(), DEC);// get the current year
-  Serial.print('/');
-  Serial.print(now.month(), DEC); //get the current month
-  Serial.print('/');
-  Serial.print(now.day(), DEC);//get the current day
-  Serial.print(" (");
-  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]); //get the date
-  Serial.print(") ");
-  Serial.print(now.hour(), DEC);//get current hour
-  Serial.print(':');
-  Serial.print(now.minute(), DEC); //get current minute
-  Serial.print(':');
-  Serial.print(now.second(), DEC); //get current second
-  Serial.println();
 }
 
 void loop() {
-//Button 1: Light BH1750 sensor
-//Button 2: Air Quality BME680 sensor
-//Button 3: Both sensor
-//Button 4: Save sensor data into SD card
-
-current = rtc.now();
-display(current);
-delay(1000);
+Serial.println("Pressing button to set sensor:\n1 - Light sensor\n2 - Air Quality sensor\n3 - Both sensors work continously\n4 - Save data to SD Card");
 
   if (digitalRead(button1) == LOW) {
     Serial.println("Button 1 pressed - BH1750 Light sensor");
     Light();
-
+    tft.fillScreen(ST77XX_BLACK);
+    tft.setCursor(0, 0);
+    tft.setTextColor(ST77XX_WHITE);
+    tft.setTextSize(2);
+    tft.println("Display BH1750 Light Data");
     delay(2000);
   } else if (digitalRead(button2) == LOW) {
     Serial.println("Button 2 pressed - BME680 Air Quality sensor");
     AQ();
-
+    tft.fillScreen(ST77XX_BLACK);
+    tft.setCursor(0, 0);
+    tft.setTextColor(ST77XX_WHITE);
+    tft.setTextSize(2);
+    tft.println("Display BME680 Air Quality Data");
     delay(2000);
   } else if (digitalRead(button3) == LOW) {
     Serial.println("Button 3 pressed - Both sensors work continuously");
     Both();
-
+    tft.fillScreen(ST77XX_BLACK);
+    tft.setCursor(0, 0);
+    tft.setTextColor(ST77XX_WHITE);
+    tft.setTextSize(2);
+    tft.println("Displaying Both Sensor Data");
+    delay(2000);
   } else if (digitalRead(button4) == LOW) {
     Serial.println("Button 4 pressed - Saving Sensor Data into SD card");
-
-  // Read sensor data from BH1750 and BME680
-    String sensordata = "";
-    float lightData = lightMeter.readLightLevel();
-    String aqData = getAQData();
-
-    // Store sensor data in sensordata variable
-    sensordata = "Light: " + String(lightData) + " lx\n" + "Air Quality: " + aqData;
-
-    // Save sensor data to SD card
-    WriteSD(file, sensordata);
-
-    Serial.println("Data saved into sensor_data.txt");
-    tft.fillScreen(ST77XX_BLACK);
-    tft.setCursor(5, 50);
-    tft.setTextColor(ST77XX_WHITE);
-    tft.setTextSize(1);
-    tft.println("Data saved into sensor_data.txt");
-    delay(2000);
+    File dataFile = SD.open("data.txt", FILE_WRITE);
+    if (dataFile) {
+      dataFile.print("Light: ");
+      dataFile.print(lightMeter.readLightLevel());
+      dataFile.print(" lux - ");
+      if (bme.performReading()) {
+        dataFile.print("Temperature: ");
+        dataFile.print(bme.temperature);
+        dataFile.print(" *C, ");
+        dataFile.print("Humidity: ");
+        dataFile.print(bme.humidity);
+        dataFile.print(" %, ");
+        dataFile.print("Pressure: ");
+        dataFile.print(bme.pressure / 100.0);
+        dataFile.println(" hPa");
+      } else {
+        dataFile.println("Failed to read BME680 sensor!");
+      }
+      dataFile.close();
+      Serial.println("Data saved into data.txt");
+      tft.fillScreen(ST77XX_BLACK);
+      tft.setCursor(0, 0);
+      tft.setTextColor(ST77XX_WHITE);
+      tft.setTextSize(2);
+      tft.println("Data saved into data.txt");
     }
   }
-
-
-void WriteSD(FsFile& file, const String& sensordata) {
-  Serial.println("---   Saving To File   ---");
-
-  if (!file.open("sensor_data.txt", O_RDWR | O_CREAT | O_APPEND)) {
-    sd.errorHalt(F("open failed"));
-  }
-
-  file.println(sensordata);
-
-  file.close();
 }
 
-
-void ReadSD(File file) {
-  Serial.println("--- Reading From file! ---");
-  
-  file.open("sensor_data.txt", O_RDWR);
-  file.seek(0);                 //go to char 0
-  file.close();
-}
 
 void AQ() {
   if (! bme.performReading()) {
@@ -295,6 +222,7 @@ float lux = lightMeter.readLightLevel();
   Serial.print(lux);
   Serial.println(" lx");
   Serial.println();
+
 
   tft.fillScreen(ST77XX_BLACK);
   tft.setTextSize(1);
@@ -405,6 +333,7 @@ float lux = lightMeter.readLightLevel();
   tft.setTextColor(ST77XX_WHITE);
   tft.println(" lx");
 }
+
 
 void resetFunc() {
   tft.fillScreen(ST77XX_WHITE);
